15521e86a78df5028494bf5f24d63a4ebef2bd56
Joaquin Uribe <juribe@nursoft.cl>
2022-08-29 15:45:05 -0400

    Organizing nodes

---
+++ Scenes/key.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func _input(ev):
+	if ev is InputEventKey and ev.scancode == KEY_1:
+		#trigger animation
+		print("Hola")
+		pass
