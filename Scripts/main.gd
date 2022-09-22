3f68ec23382487328a2bf59c7cd93a4cdb11edbf
Joaquin Uribe <juribe@nursoft.cl>
2022-09-22 11:44:03 -0300

    Created keyboard scene for the demo

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -3,7 +3,6 @@ extends Node2D
 var tile_size = 32
 export(int) var player_speed = 30
 onready var player = get_node("player")
-onready var tilemap = get_node("tileMap")
 var directions = {
 	"up": Vector2.UP,
 	"down": Vector2.DOWN,
@@ -31,17 +30,8 @@ func _on_key_pressed(pos):
 func _ready():
 	for key in keys.get_children():
 		key.connect("key_pressed", self, "_on_key_pressed")
-	
-func getGridPosition(coordinate):
-	return tilemap.world_to_map(tilemap.to_local(coordinate))
 
-func getTileId(gridPos):
-	return tilemap.get_cellv(gridPos)
-
-
-var count = 0
 func _process(delta):
-
 	if (current_objective - player.position).length() > 0.8:
 		player.position += (
 			(current_objective - player.position).normalized() 
@@ -51,28 +41,3 @@ func _process(delta):
 			movement_index += 1
 			current_objective = path[movement_index]
 	
-#	# esto es para mover el mono libremente y debugear
-#	if Input.is_action_just_pressed("up"):
-#		player.position += Vector2.UP * tile_size
-#	if Input.is_action_just_pressed("down"):
-#		player.position += Vector2.DOWN * tile_size
-#	if Input.is_action_just_pressed("left"):
-#		player.position += Vector2.LEFT * tile_size
-#	if Input.is_action_just_pressed("right"):
-#		player.position += Vector2.RIGHT * tile_size
-#
-#	var tileIds = tilemap.getIds()
-#	var playerGridPos = getGridPosition(player.position)
-#	var adjacentTileIds = {
-#		"up": getTileId(playerGridPos - Vector2.UP),
-#		"down": getTileId(playerGridPos - Vector2.DOWN),
-#		"left": getTileId(playerGridPos - Vector2.LEFT),
-#		"right": getTileId(playerGridPos - Vector2.RIGHT)
-#	}
-
-	
-#	for dir in adjacentTileIds:
-#		var key = tilemap.findTileKeyById(adjacentTileIds[dir])
-#		if key != "NOTFOUND" and Input.is_action_just_pressed(key):
-#			player.position -= directions[dir] * tile_size
-#			break	
--- Scripts/player_script.gd
+++ Scripts/player_script.gd
@@ -1,17 +1,4 @@
 extends KinematicBody2D
 
-var tile_size = 32
-
-func _ready():
-	position = position.snapped(Vector2.ONE * tile_size)
-	position += Vector2.ONE * tile_size/2
-
-func read_input():
-	pass
-
-	
-func _physics_process(delta):
-	read_input()
-
 func take_damage():
 	print("oh no!")
