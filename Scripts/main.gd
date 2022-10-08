bd77cbffd3b82724c9cf33de93cc1b89a781f1f7
Geral <geraldyn37@gmail.com>
2022-10-08 19:54:26 -0400

    animacion de correr

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -3,6 +3,8 @@ extends Node2D
 export(int) var player_speed = 30
 export(int) var run_speed = 100
 onready var player = get_node("player")
+onready var anim_tree = player.get_node("AnimationTree")
+onready var playback = anim_tree.get("parameters/playback")
 var directions = {
 	"up": Vector2.UP,
 	"down": Vector2.DOWN,
@@ -30,20 +32,27 @@ func _on_key_pressed(pos):
 func _ready():
 	for key in keys.get_children():
 		key.connect("key_pressed", self, "_on_key_pressed")
+	anim_tree.active = true
+	
 
 func _process(delta):
 	if (current_objective - player.position).length() > 0.8:
 		player.position = player.position.move_toward(current_objective, player_speed * delta)
-		player.get_node("AnimationPlayer").play('Walking')
+		
 	else:
 		if len(path) >= movement_index + 2:
 			movement_index += 1
 			current_objective = path[movement_index]
 		else:
-			player.get_node("AnimationPlayer").play("RESET")
+			playback.travel('Idle')
 	
 func _input(_event):
 	if Input.is_key_pressed(KEY_SHIFT):
 		player_speed = run_speed
+		if len(path) >= movement_index + 2:
+			playback.travel('Run')
+		else:
+			pass
 	else:
-		player_speed = original_speed 
+		player_speed = original_speed
+		playback.travel('Walking')
