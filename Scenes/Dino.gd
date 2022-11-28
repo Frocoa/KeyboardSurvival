98609f8121866f0d969f9b9245826d5064fb0c18
Joaquin Uribe <juribe@nursoft.cl>
2022-11-28 11:13:53 -0300

    Fix fullscreen

--- Scenes/Dino.gd
+++ Scenes/Dino.gd
@@ -8,8 +8,8 @@ export(int) var dino_offset = 50
 # var b = "text"
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	translate(Vector2(5,0))
+func _process(delta):
+	translate(Vector2(250,0) * delta)
 	
 	if position.x > get_tree().get_root().size.x + dino_offset:
 		position.x = position.x - (get_tree().get_root().size.x + (dino_offset * 5))
